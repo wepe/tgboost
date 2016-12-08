a0610161fc13e97b61a0b7849c9a5cf6aefc239c
wepon <masterwepon@163.com>
2016-12-08 14:49:58 +0800

    implement mae,mse, etc

--- tgboost/metric.py
+++ tgboost/metric.py
@@ -9,8 +9,17 @@ def error(preds, labels):
     return 1.0 - accuracy(preds,labels)
 
 
+def mean_square_error(preds, labels):
+    return np.mean(np.square(preds - labels))
+
+
+def mean_absolute_error(preds, labels):
+    return np.mean(np.abs(preds - labels))
+
 metrics = {"acc": accuracy,
-           "error": error}
+           "error": error,
+           "mse": mean_square_error,
+           "mae": mean_absolute_error}
 
 
 def get_metric(eval_metric):
